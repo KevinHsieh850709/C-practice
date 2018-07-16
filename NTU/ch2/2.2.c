90592fa8c1aa2565b46f905e9d7322c0dc44038b
Kevin Hsieh <kevinhsieh850709@gmail.com>
2018-07-16 17:24:32 +0800

    Finish ch2

---
+++ NTU/ch2/2.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int x;
+    scanf("%d", &x);
+    printf("%d\n%d\n", x, x);
+    system("pause");
+}
---
+++ NTU/ch2/2.3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int x,y;
+    scanf("%d%d", &x, &y);
+    printf("%d\n", y);
+    system("pause");
+}
